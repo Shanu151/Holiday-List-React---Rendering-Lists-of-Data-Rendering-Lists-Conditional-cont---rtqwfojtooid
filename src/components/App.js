5a9cda4726a71c076ceca92ff1afec41070e7c41
Shanu151 <77886773+Shanu151@users.noreply.github.com>
2022-08-10 15:27:47 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,11 @@
-
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+function App() {
+   
+    
 
-    this.cityList = [{ name: 'Goa', country: 'India' },
+    const cityList = [{ name: 'Goa', country: 'India' },
     { name: 'Amsterdam', country: 'Netherlands' },
     { name: 'New York', country: 'USA' },
     { name: 'Darjeeling', country: 'India' },
@@ -35,16 +34,25 @@ class App extends Component {
     { name: 'Mount Abu', country: 'India' },
     { name: 'Tirupati', country: 'India' },
     ]
-  }
+  
+   
 
-  render() {
+  
     return (
       <div id="main">
         {/* Do not remove the main div */}
+        <ol>
+            {cityList.filter((cout)=> cout.country === 
+            'India').map((city, index)=>{
+              return(
+                <li key={`location${index+1}`}>{city.name}</li>
+              )
+            })}
+        </ol>
       </div>
     )
   }
-}
+
 
 
 export default App;
